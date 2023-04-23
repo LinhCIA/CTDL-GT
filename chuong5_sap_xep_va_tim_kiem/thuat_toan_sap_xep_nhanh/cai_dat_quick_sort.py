f2b48cf7c6261ce1473e524268533542c44410cc
LINH AI <119838869+LinhCIA@users.noreply.github.com>
2023-04-23 17:42:28 +0700

    Update cai_dat_quick_sort.py

--- chuong5_sap_xep_va_tim_kiem/thuat_toan_sap_xep_nhanh/cai_dat_quick_sort.py
+++ chuong5_sap_xep_va_tim_kiem/thuat_toan_sap_xep_nhanh/cai_dat_quick_sort.py
@@ -1,31 +1,25 @@
 # Mã nguồn python thuật toán sắp xếp (quick sort)
 
 
-def vach_ngan(arr, l, r, p):   # Kí hiệu: p = pivot = trục chia; l = left = p.tử bên trái; r = right = p.tử bên phải
-    while l <= r:
-        # Tìm phần tử bên trái cần được đổi chỗ
-        while arr[l] < p:
-            l += 1
-        # Tìm phần tử bên phải cần được đổi chỗ
-        while arr[r] > p:
-            r -= 1
-        # Nếu phần tử bên trái lớn hơn phần tử bên phải, đổi chỗ 2 phần tử đó
-        if l <= r:
-            arr[l], arr[r] = arr[r], arr[l]
-            l += 1
-            r -= 1
-    # Trả về chỉ số của phần tử chốt
-    return l
+def sx_quick_sort(arr, low, high):
+    if low < high:
+        # Chọn phần tử chốt
+        pivot = arr[high]
+        # Phân vùng mảng thành 3 phần: nhỏ hơn pivot, bằng pivot, lớn hơn pivot
+        i, j, k = low, low, high
+        while j <= k:
+            if arr[j] < pivot:
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+                j += 1
+            elif arr[j] == pivot:
+                j += 1
+            else:
+                arr[j], arr[k] = arr[k], arr[j]
+                k -= 1
+        # Đệ quy sắp xếp các phần tử nhỏ hơn và lớn hơn pivot
+        sx_quick_sort(arr, low, i - 1)
+        sx_quick_sort(arr, k + 1, high)
+    return arr
 
-
-def sx_quick_sort(arr, l, r):
-    if l < r:
-        # Chọn phần tử chốt p là phần tử giữa
-        p = arr[(l + r) // 2]
-        # Phân hoạch mảng và lấy chỉ số phần tử chốt
-        index = vach_ngan(arr, l, r, p)
-        # Gọi đệ quy sắp xếp phần mảng trước phần tử chốt
-        sx_quick_sort(arr, l, index - 1)
-        # Gọi đệ quy sắp xếp phần mảng sau phần tử chốt
-        sx_quick_sort(arr, index, r)
 
