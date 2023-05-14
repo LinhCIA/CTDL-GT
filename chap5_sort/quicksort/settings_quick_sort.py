fa3d7e5d98ca0a60dd6252343e5f7062882af348
Lê Hùng Anh <119838869+LinhCIA@users.noreply.github.com>
2023-05-14 22:27:28 +0700

    Chương 5: Sắp xếp

--- chap5_sort/quicksort/settings_quick_sort.py
+++ chap5_sort/quicksort/settings_quick_sort.py
@@ -4,8 +4,8 @@ def sx_quick_sort(arr):
     if len(arr) <= 1:
         return arr
     # Nếu không thì chia ra 3 phần nhỏ hơn, bằng với, và lớn hơn pivot để trị (để sắp xếp)
-    pivot = arr[0]                                              # Chọn phần tử pivot(thường là phần tử đầu tiên, cuối cùng hoặc ở giữa nhưng ở đây tôi chọn pivot là phần tử đầu tiên)
-    left = [x for x in arr if x < pivot]                        # Phần bên trái chứa các phần tử nhỏ hơn pivot
-    middle = [x for x in arr if x == pivot]                     # Phần ở giữa chứa phần tử bằng với pivot
-    right = [x for x in arr if x > pivot]                       # Phần bên phải chứa các phần tử lớn hơn pivot
+    pivot = arr[0]                                     # Chọn phần tử pivot(thường là phần tử đầu tiên, cuối cùng hoặc ở giữa nhưng ở đây tôi chọn pivot là phần tử đầu tiên)
+    left = [x for x in arr if x < pivot]               # Phần bên trái chứa các phần tử nhỏ hơn pivot
+    middle = [x for x in arr if x == pivot]            # Phần ở giữa chứa phần tử bằng với pivot
+    right = [x for x in arr if x > pivot]              # Phần bên phải chứa các phần tử lớn hơn pivot
     return sx_quick_sort(left) + middle + sx_quick_sort(right)  # Vừa đệ quy sắp xếp hai dãy con lại vừa tổng hợp hai dãy con lại thành dãy đầu ra hoàn chỉnh có dạng là LpR
