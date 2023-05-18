f7f699561ffa0b251838124eda88399ce00513ba
Lê Hùng Anh <119838869+LinhCIA@users.noreply.github.com>
2023-05-18 07:02:49 +0700

    Chương 5: Sắp xếp

--- chap5_sort/quicksort/settings_quick_sort.py
+++ chap5_sort/quicksort/settings_quick_sort.py
@@ -2,7 +2,7 @@
 def sx_quick_sort(arr):
     # Nếu dãy không quá một phần tử => trả về dãy mà không cần làm gì cả
     if len(arr) <= 1:
-        return 1 
+        return arr 
     # Nếu không thì chia ra 3 phần nhỏ hơn, bằng với, và lớn hơn pivot để trị (để sắp xếp)
     pivot = arr[len(arr)//2]                      # Chọn phần tử pivot(thường là phần tử đầu tiên, cuối cùng hoặc ở giữa nhưng ở đây tôi chọn pivot là phần tử trung tâm)
     left = [x for x in arr if x < pivot]          # Phần bên trái chứa các phần tử nhỏ hơn pivot
